c233596db6e1acc0780fb347fc8260e72fc4661e
PezzottiCarlo <carlo.pezzotti@student.supsi.ch>
2022-03-02 15:36:17 +0100

    Upload menu class and Interface🛠️

---
+++ src/Menu/Menu.java
@@ -0,0 +1,17 @@
+package Menu;
+
+import java.util.Scanner;
+
+public class Menu implements MenuInterface {
+    public static Scanner scanner;
+    
+    public Menu() {
+        scanner = new Scanner(System.in);
+    }
+
+    @Override
+    public void execute(int choice) {
+        // TODO Auto-generated method stub
+        
+    }
+}
