20ffb03929ff018e6b1a95bd2e0bea5a3590c1ef
PezzottiCarlo <carlo.pezzotti@student.supsi.ch>
2022-03-02 10:54:47 +0100

    Upload class Giocatore and Dado 🛠

--- src/Giocatore.java
+++ src/Giocatore.java
@@ -1,2 +1,18 @@
 public class Giocatore {
+    private String username;
+    private Point coordinate;
+    public final String CHARACTER = "X";
+
+    public Giocatore(String username, Point coordinate) {
+        this.username = username;
+        this.coordinate = coordinate;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public Point getCoordinate() {
+        return coordinate;
+    }
 }
