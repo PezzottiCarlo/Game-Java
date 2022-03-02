e4f0ff3dec0edf936f73990ae34dc4ca481d2c25
Arena Matteo <matteo.arena@student.supsi.ch>
2022-03-02 11:33:59 +0100

    Grazie alla classe Partita si può uscire dal gioco o mostrare la tavola di gioco

--- src/Partita.java
+++ src/Partita.java
@@ -1,2 +1,36 @@
+import java.util.Scanner;
+
 public class Partita {
+    public static final int BOARD_SIZE = 7;
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        boolean inGame = true;
+        TavoloDaGioco gameBoard = new TavoloDaGioco(7);
+
+
+        while(inGame){
+            int choose = RappresentazioneTestuale.askWhatToDo(input);
+            switch (choose){
+                case 0:
+                    input.close();
+                    inGame = false;
+                    return;
+                case 1:
+                    gameBoard.displayGrid();
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                default:
+                    System.out.println("Funzionalità ancora non implementata");
+            }
+        }
+
+
+
+
+
+    }
 }
--- src/RappresentazioneTestuale.java
+++ src/RappresentazioneTestuale.java
@@ -1,15 +1,13 @@
 import java.util.Scanner;
-
 public class RappresentazioneTestuale {
     private static final String[] options = {"Esci", "Mostra Griglia", "Muovi", "Mostra info giocatori"};
 
-    public static int askWhatToDo() {
+    public static int askWhatToDo(Scanner in) {
         for (int i = 0; i < options.length; i++) {
             System.out.println(i + "." + options[i]);
         }
 
         int num = Integer.MIN_VALUE;
-        Scanner in = new Scanner(System.in);
         System.out.print("Scelta: ");
         do {
             if(num != Integer.MIN_VALUE){
@@ -21,7 +19,6 @@ public class RappresentazioneTestuale {
             }
             num = in.nextInt();
         }while(num < 0 || num >= options.length);
-        in.close();
         return num;
     }
 
