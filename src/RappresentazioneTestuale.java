c4ad6fab5c6337806f551420b2a4b0c8654d1afe
Arena Matteo <matteo.arena@student.supsi.ch>
2022-03-02 11:19:28 +0100

    RappresentazioneTestuale Finita: Piccolo bug, ogni tanto stampa due volte la scritta "Valore non accettabile"

--- src/RappresentazioneTestuale.java
+++ src/RappresentazioneTestuale.java
@@ -1,23 +1,26 @@
 import java.util.Scanner;
 
 public class RappresentazioneTestuale {
-    private static String[] options = {"Esci", "Mostra Griglia", "Muovi", "Mostra info giocatori"};
+    private static final String[] options = {"Esci", "Mostra Griglia", "Muovi", "Mostra info giocatori"};
 
     public static int askWhatToDo() {
         for (int i = 0; i < options.length; i++) {
             System.out.println(i + "." + options[i]);
         }
 
-        int num = -1;
+        int num = Integer.MIN_VALUE;
         Scanner in = new Scanner(System.in);
+        System.out.print("Scelta: ");
         do {
-            System.out.print("Scelta: ");
+            if(num != Integer.MIN_VALUE){
+                System.out.println("Valore fuori dal range [0," + options.length + "] ");
+            }
             while (!in.hasNextInt()) {
-                System.out.print("Scelta:");
+                System.out.println("Valore non accettabile");
                 in.nextLine();
             }
             num = in.nextInt();
-        }while(num >= 0 && num < options.length);
+        }while(num < 0 || num >= options.length);
         in.close();
         return num;
     }
