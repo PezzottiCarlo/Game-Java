7787c4d227132e3a8a807daf23c20be0bb54e9b7
PezzottiCarlo <carlo.pezzotti@student.supsi.ch>
2022-03-07 15:22:41 +0100

    feat: Inline menu

--- src/Menu/Menu.java
+++ src/Menu/Menu.java
@@ -6,25 +6,41 @@ import java.util.Scanner;
 
 import Menu.Option.IOption;
 
-public class Menu{
+public class Menu {
     public static Scanner scanner;
     private List<IOption> menuItems;
-    
+    private boolean inline;
+
     static {
         scanner = new Scanner(System.in);
     }
 
     public Menu(boolean inline) {
+        this.inline = inline;
         menuItems = new ArrayList<>();
     }
 
-    public void ask(){
+    public void ask() {
         int choice = -1;
         while (choice != 0) {
-            for (int i = 0; i < menuItems.size(); i++) {
-                System.out.println(i + "." + menuItems.get(i).toString());
+            if (!inline) {
+                for (int i = 0; i < menuItems.size(); i++) {
+                    System.out.println(i + "." + menuItems.get(i).toString());
+                }
+                System.out.print("Scelta: ");
+            } else {
+                System.out.print("Choose an option: ");
+                // print option
+                for (int i = 0; i < menuItems.size(); i++) {
+                    if (i == 0)
+                        System.out.print("[");
+                    if (i >= 0 && i < menuItems.size() - 1)
+                        System.out.print(i + ": "+ menuItems.get(i) + ", ");
+                    if (i == menuItems.size()-1)
+                        System.out.print(i + ": "+menuItems.get(i)+"]: ");
+                }
             }
-            System.out.print("Scelta: ");
+
             while (!scanner.hasNextInt()) {
                 System.out.println("Valore non accettabile");
                 scanner.nextLine();
