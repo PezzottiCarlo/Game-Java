3f0b5b9856d00a8f0606a341210fd2cb8792acc7
Arena Matteo <matteo.arena@student.supsi.ch>
2022-03-08 11:37:57 +0100

    Refactor Coin

--- src/GameObjects/Coin.java
+++ src/GameObjects/Coin.java
@@ -4,13 +4,32 @@ import General.*;
 
 public class Coin extends GameObject{
 
+    //==================== Attributes ===================
+
+    /**
+     * Character representing the coin.
+     */
     public static final char COIN_CHARACTER = '$';
 
+    // ==================== Constructors ====================
+
+    /**
+     * Constructor for the coin.
+     *
+     * @param position The position of the coin.
+     */
     public Coin(Point position) {
         super(position, COIN_CHARACTER, new Color(Color.ANSI_YELLOW));
     }
 
+    /**
+     * Constructor for the coin.
+     *
+     * @param x The x coordinate of the coin.
+     * @param y The y coordinate of the coin.
+     */
     public Coin(int x, int y){
         super(new Point(x,y),COIN_CHARACTER,new Color(Color.ANSI_YELLOW));
     }
+
 }
